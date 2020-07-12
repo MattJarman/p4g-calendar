3bcd9100874bd8309dfde01bf619160d7cc9be0a
matt <mattjarman22@outlook.com>
2020-07-12 13:39:52 +0100

    Add gh-pages, Reduce tailwind css file size

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,4 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
 const tailwindcss = require('tailwindcss');
 module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), require('autoprefixer')],
+  plugins: [
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer'),
+    cssnano({ preset: 'default' }),
+    purgecss({
+      content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.js'],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+  ],
 };
